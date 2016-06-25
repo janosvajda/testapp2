c59c0ba675063b14497f9aaf2e3773b3ea90f6c4
Janos Vajda <vajdajanos@gmail.com>
2016-06-25 13:28:50 +0100

    third commit

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
-var http = require('http'),
-    fs = require('fs');
+var express = require('express'), 
+    app = express(),
+    http = require('http'),
+    io = require('socket.io');
+    
+var server = http.createServer(app);
+var io = io.listen(server);
+server.listen(process.env.PORT || 8888);
+app.use(express.static(__dirname + '/public'));
+console.log("Server running on 127.0.0.1:8080");
 
+// array of all lines drawn
+var line_history = [];
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(process.env.PORT || 8888);
+// event-handler for new incoming connections
+io.on('connection', function (socket) {
+
+   // first send the history to the new client
+   for (var i in line_history) {
+      socket.emit('draw_line', { line: line_history[i] } );
+   }
+
+   // add handler for message type "draw_line".
+   socket.on('draw_line', function (data) {
+      // add received line to history 
+      line_history.push(data.line);
+      // send line to all clients
+      io.emit('draw_line', { line: data.line });
+   });
 });
