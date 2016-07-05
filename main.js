9a3a3308036feb5106f8f7868299a7441dad1219
Janos Vajda <vajdajanos@gmail.com>
2016-07-05 14:18:49 +0100

    Added EasyRTC module to project.

--- main.js
+++ main.js
@@ -2,10 +2,20 @@ var express = require('express'),
     app = express(),
     http = require('http'),
     io = require('socket.io');
+
+var easyrtc = require("easyrtc");           // EasyRTC external module
     
 var server = http.createServer(app);
+var server2 = http.createServer(app);
+
 var io = io.listen(server);
+
+app.use("/js", express.static(__dirname + '/easyrtc/js'));
+app.use("/images", express.static(__dirname + '/easyrtc/images'));
+app.use("/css", express.static(__dirname + '/easyrtc/css'));
+
 server.listen(process.env.PORT || 8888);
+
 app.use(express.static(__dirname + '/public'));
 console.log("Server running on 127.0.0.1:8080");
 
@@ -28,3 +38,14 @@ io.on('connection', function (socket) {
       io.emit('draw_line', { line: data.line });
    });
 });
+
+
+//Initiate a video call
+app.get('/video', function(req, res){
+	var easyrtcServer = easyrtc.listen(app, socketServer, {'apiEnable':'true'});
+        res.sendfile(__dirname + '/easyrtc/demo_multiparty.html');
+
+});
+
+
+var socketServer = io.listen(server2);
